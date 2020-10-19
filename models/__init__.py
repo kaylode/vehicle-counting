ee3623d646e08937897d5fcf629c6e0294d45d5f
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2020-10-19 23:13:43 +0700

    clean code

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,2 @@
 from .base_model import BaseModel
-from .classifier import Classifier
 from .detector import Detector
--- models/efficientdet/model.py
+++ models/efficientdet/model.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch
 from torchvision.ops.boxes import nms as nms_torch
 
-from efficientnet import EfficientNet as EffNet
-from efficientnet.utils import MemoryEfficientSwish, Swish
-from efficientnet.utils_extra import Conv2dStaticSamePadding, MaxPool2dStaticSamePadding
+from ..efficientnet import EfficientNet as EffNet
+from ..efficientnet.utils import MemoryEfficientSwish, Swish
+from ..efficientnet.utils_extra import Conv2dStaticSamePadding, MaxPool2dStaticSamePadding
 
 
 def nms(dets, thresh):
