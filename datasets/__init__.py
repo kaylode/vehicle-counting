1c4973e1b77819cc5f3f39e53b0a423e9255880e
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2020-10-19 23:12:31 +0700

    add coco dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,1 @@
-from .csv_classification import CSVTextClassificationDataset
-from .image_classification import ImageClassificationDataset
-from .detection import ObjectDetectionDataset
-#from .vocab import CustomVocabulary
+from .dataset import *
